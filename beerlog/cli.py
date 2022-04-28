20c222bd0d5f17fa198e9116920934b18f77089e
Letícia Lang <81568847+leticiaplang@users.noreply.github.com>
2022-04-28 12:18:12 -0300

    Update cli.py

--- beerlog/cli.py
+++ beerlog/cli.py
@@ -1,5 +1,24 @@
-from .config import settings
+import typer
+from beerlog.core import add_beer_to_database, get_beers_from_database
 
+main = typer.Typer(help="Beer Management Application")
 
-def main():
-    print("Hello from", settings.NAME)
+@main_command('add')
+def add(
+    name: str, 
+    style: str,
+    flavor: int = typer.Option(...),
+    image: int = typer.Option(...),
+    cost: int = typer.Option(...)
+    ):
+    '''Adds a new beer to database.'''
+    if add_beer_to_database(name, style, flavor, image, cost):
+        print("Beer added to database.")
+    else:
+        print("Something went wrong.")
+
+@main_command('list')
+def list_beers(style: str) -> List[Beer]:
+    '''Lists beers in database.'''
+    beers = get_beers_from_database()
+    print(beers)
